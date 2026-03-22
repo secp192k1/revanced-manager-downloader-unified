5059673197c9b22726408c21183a4e4e34ff1d82
Kofhisho <github@alphexo.dev>
2026-03-22 21:42:58 +0000

    fix(FDroid): Init

--- src/main/kotlin/app/revanced/manager/downloaders/unified/fdroid/FDroidUniDownloader.kt
+++ src/main/kotlin/app/revanced/manager/downloaders/unified/fdroid/FDroidUniDownloader.kt
@@ -2,12 +2,15 @@
 
 package app.revanced.manager.downloaders.unified.fdroid
 
-import android.net.Uri
+import android.util.Log
 import app.revanced.manager.downloader.DownloadUrl
 import app.revanced.manager.downloader.Downloader
 import app.revanced.manager.downloader.download
-import app.revanced.manager.downloader.webview.runWebView
 import app.revanced.manager.downloaders.R
+import app.revanced.manager.downloaders.unified.data.OkClient
+import app.revanced.manager.downloaders.unified.data.Provider
+import io.ktor.client.statement.*
+import io.ktor.utils.io.*
 import java.nio.file.Files
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.deleteRecursively
@@ -15,31 +18,46 @@ import kotlin.io.path.deleteRecursively
 @OptIn(ExperimentalPathApi::class)
 val FDroidUniDownloader = Downloader(R.string.fdroid_uni) {
     get { packageName, version ->
-        runWebView("F-Droid ★") {
-            download { url, _, userAgent ->
-                finish(
-                    DownloadUrl(
-                        url,
-                        mapOf("User-Agent" to userAgent)
-                    )
-                )
-            }
+        Log.i("FDroidUniDownloader", "Get requested for packageName=$packageName, version=$version")
+        val pack = version?.let { "${packageName}_$it" } ?: packageName
+        val downloadUrl = "https://f-droid.org/repo/$pack.apk"
+        Log.i("FDroidUniDownloader", "DownloadUrl resolved: $downloadUrl")
 
-            Uri.Builder()
-                .scheme("https")
-                .authority("f-droid.org")
-                .path("/repo/${packageName}_$version.apk")
-                .toString()
-        } to version
+        DownloadUrl(
+            downloadUrl,
+            mapOf(
+                "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language" to "en-US,en;q=0.9",
+                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
+                "Connection" to "keep-alive",
+                "DNT" to "1",
+                "Referer" to "https://f-droid.org/"
+            )
+        ) to version
     }
 
     download { downloadUrl, outputStream ->
         val workingDir = Files.createTempDirectory("fdroid_uni_dl")
         try {
-            val (inputStream, size) = downloadUrl.toDownloadResult()
-            inputStream.use {
-                if (size != null) reportSize(size)
-                it.copyTo(outputStream)
+            Log.i("FDroidUniDownloader", "Starting download stream for: ${downloadUrl.url}")
+            OkClient.fetchStream(Provider.F_DROID, downloadUrl.url) { response ->
+                if (response.status.value !in 200..299) {
+                    val msg = "Download HTTP failed: ${response.status.value}"
+                    Log.e("FDroidUniDownloader", msg)
+                    throw Exception(msg)
+                }
+
+                val contentLength = response.headers["Content-Length"]?.toLongOrNull()
+                val channel = response.bodyAsChannel()
+                Log.i("FDroidUniDownloader", "Downloading as raw APK, Content-Length: $contentLength")
+                if (contentLength != null) reportSize(contentLength)
+
+                val buffer = ByteArray(8192)
+                while (!channel.isClosedForRead) {
+                    val read = channel.readAvailable(buffer)
+                    if (read == -1) break
+                    outputStream.write(buffer, 0, read)
+                }
             }
         } finally {
             workingDir.deleteRecursively()
